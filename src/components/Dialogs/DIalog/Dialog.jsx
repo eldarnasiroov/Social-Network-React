7929d46a2324cbbc02e3bbbfd1549d2abb5ef751
eldarnasiroov <eldarnasiroov@gmail.com>
2022-08-16 19:39:08 +0400

    last commit

--- src/components/Dialogs/DIalog/Dialog.jsx
+++ src/components/Dialogs/DIalog/Dialog.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import classes from './Dialog.module.css';
 import {NavLink} from 'react-router-dom';
 
+
 const Dialog = ({id, senderName}) => {
     return (
         <NavLink to={"/messages/" + id} className={classes.dialog}>{senderName}</NavLink>
